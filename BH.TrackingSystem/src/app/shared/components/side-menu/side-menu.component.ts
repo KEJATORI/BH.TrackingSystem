d1566dae2faad0d2738ddc9ad21177efebde8dd2
kejatori <riverakevinjames@gmail.com>
2023-01-10 16:55:46 +0800

    fix initial window size when mobile"

--- BH.TrackingSystem/src/app/shared/components/side-menu/side-menu.component.ts
+++ BH.TrackingSystem/src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,13 +1,17 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.scss']
 })
-export class SideMenuComponent {
+export class SideMenuComponent implements OnInit {
   isHidden = false;
   windowSize = window.innerWidth;
+
+  ngOnInit(): void {
+    this.onResize();
+  }
 
   @HostListener("window:resize")
   onResize(){
